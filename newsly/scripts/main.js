46b5ddcf8c110f9cb91a91030fab82fcce477f79
g <grant.j.j.scott@gmail.com>
2019-04-17 17:42:56 -0400

    added a query to get all news objects in db

--- newsly/scripts/main.js
+++ newsly/scripts/main.js
@@ -1,13 +1,34 @@
-function signOut() {
-  console.log("signing out")
-  firebase.auth().signOut().then(function () {
-    console.log('Successfully Signed out');
-  }).catch(function (error) {
-    return res.status(400).json(error);
-  });
+
+class mainpageHandler{
+
+  signOut() {
+    console.log("signing out")
+    firebase.auth().signOut().then(function () {
+      console.log('Successfully Signed out');
+    }).catch(function (error) {
+      return res.status(400).json(error);
+    });
+  }
+  getAllPosts(database){
+    console.log("querying the database");
+    
+      var query = database.ref('news/');
+      var results = [];
+      console.log("right before query")
+      query.on("child_added", function(data){
+        
+         results=console.log(data.val().date);;
+        
+});
+
+  }
+
 }
 
+handler = new mainpageHandler();
+
 window.onload = () => {
-  let listener = document.querySelector('#signout')
-  listener.addEventListener('click', signOut())
+  database = firebase.database();
+handler.getAllPosts( database);
+
 }
