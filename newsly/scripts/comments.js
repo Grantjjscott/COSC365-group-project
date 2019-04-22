e61c062b248388136d9dd2589c82a6974300325e
Zachary Zajdel <42820232+Zac-Zajdel@users.noreply.github.com>
2019-04-22 12:36:59 -0400

    Began on comments

--- newsly/scripts/comments.js
+++ newsly/scripts/comments.js
@@ -1,9 +1,20 @@
-function getComments() {
-  let query = database.ref('Feedback');
-  query.equalTo(Postkey).on("Value", function (snapshot) {
-    console.log(snapshot.val());
-    snapshot.forEach(function (data) {
-      console.log(data.key);
+class loadPost {
+
+  getComments() {
+    let query = database.ref('Feedback');
+    query.equalTo(Postkey).on("Value", function (snapshot) {
+      console.log(snapshot.val());
+      snapshot.forEach(function (data) {
+        console.log(data.key);
+      });
     });
-  });
+  }
+}
+
+handler = new mainpageHandler();
+
+// When the window loads, getCurrentPost() will be triggered.
+window.onload = () => {
+  handler.getCurrentPost();
+  handler.getComments();
 }
