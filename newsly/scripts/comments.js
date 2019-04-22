71e5d741cb11ffa89e1694affe5bb9f099dd6de1
Zachary Zajdel <42820232+Zac-Zajdel@users.noreply.github.com>
2019-04-22 12:45:29 -0400

    More stuff

--- newsly/scripts/comments.js
+++ newsly/scripts/comments.js
@@ -1,20 +1,40 @@
-class loadPost {
+// class loadPost {
 
-  getComments() {
+//   getComments() {
+//     let query = database.ref('Feedback');
+//     query.equalTo(Postkey).on("Value", function (snapshot) {
+//       console.log(snapshot.val());
+//       snapshot.forEach(function (data) {
+//         console.log(data.key);
+//       });
+//     });
+//   }
+// }
+
+// Grabs form and adds event listener for when user submits the comment.
+const form = document.querySelector("form");
+form.addEventListener("submit", postComment);
+
+const postComment = (e) => {
+  e.preventDefault();
+  let comment = document.getElementById('comment').value;
+
+  if (name) {
     let query = database.ref('Feedback');
-    query.equalTo(Postkey).on("Value", function (snapshot) {
-      console.log(snapshot.val());
-      snapshot.forEach(function (data) {
-        console.log(data.key);
-      });
+    query.push({
+      date: new Date(),
+      text: comment,
+      user: hello // HELP
     });
   }
+
+  document.getElementById('comment').value = '';
 }
 
-handler = new mainpageHandler();
+// handler = new mainpageHandler();
 
-// When the window loads, getCurrentPost() will be triggered.
-window.onload = () => {
-  handler.getCurrentPost();
-  handler.getComments();
-}
+// // When the window loads, getCurrentPost() will be triggered.
+// window.onload = () => {
+//   handler.getCurrentPost();
+//   handler.getComments();
+// }
